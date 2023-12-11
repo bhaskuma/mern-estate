bd6900f4295b85f0ff603f2cf3962c74e59eb5bc
bhaskuma <bhaskarkumarrajput100@gmail.com>
2023-12-11 11:33:20 +0530

    create header component

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,10 +5,13 @@ const Header = () => {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between text-center max-w-6xl mx-auto p-3'>
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+          <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                 <span className='text-slate-500'>Sahand</span>
                 <span className='text-slate-700'>Estate</span>
             </h1>
+          </Link>
+            
             <form className='flex items-center rounded-lg bg-slate-100 p-3'>
             <input type='text'placeholder='Search..' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
               <FaSearch/>
