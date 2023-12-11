fa56b0a34c8b4dd35891694786373c669e410b50
bhaskuma <bhaskarkumarrajput100@gmail.com>
2023-12-11 12:14:48 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
+mongoose.connect(process.env.URL).then(() => {
+    console.log('conntect to database')
+}).catch((error) => {
+    console.log(error)
+})
 
 app.listen(3000, () => {
     console.log('server is running on 3000')
