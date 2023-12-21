3063aeca3c1f552d9d4d7ccf3523ff04883fd7c4
bhaskuma <bhaskarkumarrajput100@gmail.com>
2023-12-21 17:26:40 +0530

    add pre installion

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import userAuth from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path';
 dotenv.config()
 
 const app = express()
@@ -19,6 +20,8 @@ mongoose.connect(process.env.URL).then(() => {
     console.log(error)
 })
 
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
     console.log('server is running on 3000')
 })
@@ -26,6 +29,11 @@ app.use('/api/user', userRouter)
 app.use('/api/auth', userAuth)
 app.use('/api/listing', listingRouter)
 
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error'
